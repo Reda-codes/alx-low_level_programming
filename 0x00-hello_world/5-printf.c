aabd4d71597a678cdede6a95235b8cb6c23aacd5
Reda <muhammadredabh@gmail.com>
2022-06-02 11:57:56 +0100

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints str1
+ * Return: return program's execution status
+ */
+int main(void)
+{
+
+	char str1[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf(str1);
+
+	return (0);
+}
