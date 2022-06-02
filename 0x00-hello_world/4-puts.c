a8ac90860355bd3338b0ef337a5409216cf03058
Reda <muhammadredabh@gmail.com>
2022-06-02 11:25:24 +0100

    4. Hello, puts 0.2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main () {
-	int puts(const char "\"Programming is like building a multilingual puzzle\n");
+	char str1[] = "\"Programming is like building a multilingual puzzle\n";
+	int puts(str1);
 	return(0);
 }
