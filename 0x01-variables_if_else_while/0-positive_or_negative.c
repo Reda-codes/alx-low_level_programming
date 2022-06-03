e1ef9483643165bcf86ee43248e8039e32900da8
Reda <muhammadredabh@gmail.com>
2022-06-03 09:56:30 +0100

    1. The last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -25,7 +25,7 @@ int main(void)
 	}
 	else
 	{
-		printf("%i in positive", n);
+		printf("%i is positive", n);
 	}
 	return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,12 +3,31 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+ * main - checks if number is positive
+ * n: The number to be checked
+ * Return: return program's execution status
+ */
 int main(void)
 {
 	int n;
-
+	int b;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	b = n % 10;
+
 	/* your code goes there */
+	if (b > 5)
+	{
+		printf("Last digit of %i is %d and is greater than 5", n, b);
+	}
+	else if (b == 0)
+	{
+		printf("Last digit of %i is %d and is 0", n, b);
+	}
+	else
+	{
+		printf("Last digit of %i is %d and is less than 6 and not 0", n, b);
+	}
 	return (0);
 }
