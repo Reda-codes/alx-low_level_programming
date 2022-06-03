8ffcfeab69a99e00e7ac9e0b1721233cd80b8f15
Reda <muhammadredabh@gmail.com>
2022-06-03 10:40:40 +0100

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Prints the alphabet
+ * a: alphabet
+ * A: alphabet in capital letter
+ * Return: return program's execution status
+ */
+int main(void)
+{
+	int a = 'a';
+	int A = 'A';
+	while (a <= 'z')
+	{
+
+		putchar(a);
+		a += 1;
+	}
+	while (A <= 'Z')
+	{
+
+		putchar(A);
+		A += 1;
+	}
+	putchar('\n');
+	return (0);
+}
