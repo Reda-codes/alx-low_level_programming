793eb1c446e7076c14972f401e71e012a7129402
Reda <muhammadredabh@gmail.com>
2022-06-03 12:54:54 +0100

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Prints numbers from 0 to 9
+ * a: initial number
+ * Return: return program's execution status
+ */
+int main(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+	{
+		putchar(a);
+		a++;
+		if (a != 58)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
