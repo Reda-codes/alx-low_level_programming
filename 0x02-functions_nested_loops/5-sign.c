c93b32767e80179488eaa883421581866aa38a5e
Reda <muhammadredabh@gmail.com>
2022-06-07 11:09:34 +0100

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,24 +3,23 @@
 /**
  * print_sign  - Checks for number sign
  * @n: input
- * Return: returns the result of the check
+ * Return: returns 1 if positive, 0 if zero, -1 if negative
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar(43);
-
+		_putchar(43);
+		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar(48);
-
+		_putchar(48);
+		return (0);
 	}
-	else if (n < 0)
+	else
 	{
-		putchar(45);
-
-	};
-
+		_putchar(45);
+		return (-1);
+	}
 }
