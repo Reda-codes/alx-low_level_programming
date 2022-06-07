6fefe109783db2b555d43abb149c87574af031a7
Reda <muhammadredabh@gmail.com>
2022-06-07 09:33:59 +0100

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Checks for lowercase
+ * c: input
+ * Return: 1 if letter is lowercase or 0 if not
+ */
+int main(void)
+{
+	int _islower(int c)
+
+		{
+		if (islower(c) == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (1);
+		};
+	};
+}
