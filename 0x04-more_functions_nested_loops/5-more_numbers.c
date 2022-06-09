170a7ffa44831713eeb35cab0e4f883a932962e5
Reda <muhammadredabh@gmail.com>
2022-06-09 11:10:10 +0100

    fixed fifth task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,13 @@ void more_numbers(void)
 	int a
 	int c
 
-	for (c = 0; c <= 9; c++)
+	for (c = 0; c < 10; c++)
 	{
 		for (a = 0; a <= 14; a++)
 		{
-			_putchar(a);
+			if (a > 9)
+				_putchar((a / 10) + '0');
+				_putchar((a / 10) + '0');
 		}
 		_putchar('\n');
 	}
