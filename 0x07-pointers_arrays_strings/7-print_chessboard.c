e19a340ebc1b5ee26302ebf783af08dafbaaa3be
Reda <muhammadredabh@gmail.com>
2022-06-20 17:14:28 +0100

    6. Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,11 +8,13 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	for (int i = 0; i < 8; i++)
+	int i, j;
+
+	for (i = 0; i <= 7; i++)
 	{
-		for (int j = 0; j < 8; j++)
+		for (j = 0; j <= 7; j++)
 		{
-			_putchar(*(*(i + a) + j));
+			_putchar(a[i][j]);
 		}
 		_putchar('\n');
 	}
