2fdc8ac352bb79858eff19122d3ac201dca01aaf
Reda <muhammadredabh@gmail.com>
2022-06-22 08:52:46 +0100

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <main.h>
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * _puts_recursion - function that prints a string, followed by a new line.
+ * @s: string input
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
