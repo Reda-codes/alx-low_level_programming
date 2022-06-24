fcdf626ae0a11427f0c48d05a18cc036fa7eb1b1
Reda <muhammadredabh@gmail.com>
2022-06-24 12:28:27 +0100

    3. Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * main - function that multiplies two numbers.
+ * @argc: The number of command line arguments
+ * @argv: An array that  containing the program command line arguments
+ * Return: return program's execution status
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf("%s\n", argv[2] * argv[3]);
+	}
+	return (0);
+}
