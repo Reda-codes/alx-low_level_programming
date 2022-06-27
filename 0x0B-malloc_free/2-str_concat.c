2464de293f75ee310e7cb50bd9cc8890299cecb4
Reda <muhammadredabh@gmail.com>
2022-06-27 16:21:32 +0100

    2. He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * str_concat - function that concatenates two strings.
+ * @s1: string one
+ * @s2: string two
+ * Return: pointer to a newly allocated space in memory
+ * which contains the contents of s1, followed by the contents of s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ar;
+	unsigned int i, j;
+
+	if (s1 == NULL || s1 == NULL || strlen(s1) <= 0 || strlen(s2) <= 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		ar = malloc(sizeof(char) * (strlen(s1) + strlen(s2)));
+	}
+
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < strlen(s1); i++)
+	{
+		ar[i] = s1[i];
+	}
+
+	for (j = strlen(s1); j < strlen(s1) + strlen(s2); j++)
+	{
+		ar[j] = s2[j - strlen(s1)];
+	}
+
+	return (ar);
+}
