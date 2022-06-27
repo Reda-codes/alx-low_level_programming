6e86bd4105d81aba36cc5e2be742f6db8459722d
Reda <muhammadredabh@gmail.com>
2022-06-27 13:35:06 +0100

    1. The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,9 +5,7 @@
  * _strdup - function that returns a pointer
  * to a newly allocated space in memory,
  * which contains a copy of the string given as a parameter.
- *
  * @str: string input
- *
  * Return: Returns NULL if str = NULL
  * On success, the _strdup function returns a pointer
  * to the duplicated string.
