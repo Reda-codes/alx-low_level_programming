5dab05e43b6175c3a480d8c7cb6d876f17d504fc
Reda <muhammadredabh@gmail.com>
2022-06-30 11:23:39 +0100

    fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,9 +7,8 @@
  * @s1: string one
  * @s2: string two
  * @n: number of bytes of s2
- * Return: pointer to the newly allocated space in memory
- * which contains s1, followed by the first n bytes of s2
- * and null terminated
+ * Return: pointer to the newly allocated space in memory which
+ * contains s1, followed by the first n bytes of s2 and null terminated
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -18,13 +17,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 	{
-		s1 = malloc(0);
-		s1[0] = '\0';
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		s2 = malloc(0);
-		s2[0] = '\0';
+		s2 = "";
 	}
 	if (n >= strlen(s2))
 	{
@@ -52,5 +49,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		for (j = strlen(s1); j < strlen(s1) + n; j++)
 			ar[j] = s2[j - strlen(s1)];
 	}
+	ar[strlen(s1) + n] = '\0';
 	return (ar);
 }
