2ea356e7b9dafda55e099e9f3218be51febc373a
Reda <muhammadredabh@gmail.com>
2022-07-04 10:43:33 +0100

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - function that prints the name of the file
+ * it was compiled from, followed by a new line.
+ * Return: return program's execution status
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
