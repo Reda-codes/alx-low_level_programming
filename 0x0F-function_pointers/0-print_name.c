501c850faffdce82936826729a8534a869a86c88
Reda <muhammadredabh@gmail.com>
2022-07-06 10:45:30 +0100

    added when null is passed to function case

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
  * print_name - function that prints a name.
@@ -8,5 +7,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
+
 	f(name);
 }
