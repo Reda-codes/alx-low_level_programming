2c694965e2c8970bfa6954290d5ef5b4458c9199
Reda <muhammadredabh@gmail.com>
2022-07-14 12:35:09 +0100

    fixed betty styling

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * list_len - function that returns the number of elements in a linked list_t list.
+ * list_len - function that returns the number of elements
+ * in a linked list_t list.
  * @h: structure input pointer
  * Return: the number of nodes
  */
