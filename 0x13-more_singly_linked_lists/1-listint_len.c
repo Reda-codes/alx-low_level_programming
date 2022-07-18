e20039e6fdf928a9edda6e9fc050e35c7a53cb8b
Reda <muhammadredabh@gmail.com>
2022-07-18 09:04:02 +0100

    1. List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 
 /**
- * print_listint - function that prints all the elements of a listint_t list.
- * @h: structure inpus pointer
+ * listint_len - Write a function that returns the number of elements
+ * in a linked listint_t list.
+ * @h: structure input pointer
  * Return: the number of nodes
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t nodes = 0;
 	const listint_t *node = h;
 
 	while (node != NULL)
 	{
-		printf("%d\n", node->n);
 		node = node->next;
 		nodes++;
 	}
