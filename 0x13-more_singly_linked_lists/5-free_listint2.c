ba87b157c47061976e1a1519bc3e18aaaa00e206
Reda <muhammadredabh@gmail.com>
2022-07-19 09:26:14 +0100

    fixed betty styling

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,15 +8,15 @@
  */
 void free_listint2(listint_t **head)
 {
-        listint_t *node;
+	listint_t *node;
 
-        if (head == NULL)
+	if (head == NULL)
 		return;
 
-        while (*head != NULL)
-        {
-                node = *head;
-                *head = (*head)->next;
-                free(node);
-        }
+	while (*head != NULL)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node);
+	}
 }
